49c8000db0d72d66e35595a91038fe1a66cbfd92
hari-testpress <hari@testpress.in>
2024-01-31 08:39:27 +0530

    Setup turbo

--- frontend/src/application/turbo_drive.js
+++ frontend/src/application/turbo_drive.js
@@ -1,5 +1,6 @@
 // This is the scss entry file
 import "../styles/turbo_drive.scss";
+import "@hotwired/turbo";
 
 window.document.addEventListener("DOMContentLoaded", function () {
   window.console.log("dom ready 1");
