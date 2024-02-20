e9bc8bd3f66787e3c5bfb7a085387de2d3fb3a7e
hari-testpress <hari@testpress.in>
2024-02-20 13:08:04 +0530

    Add support to update/delete the task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path               # update
-from .views import create_view, list_view
+from .views import create_view, list_view, update_view, detail_view, delete_view
 
 
 app_name = 'tasks'
@@ -7,4 +7,7 @@ app_name = 'tasks'
 urlpatterns = [
     path('', list_view, name='list'),
     path('create/', create_view, name='create'),
+    path('<int:pk>/', detail_view, name='detail'),
+    path('<int:pk>/delete', delete_view, name='delete'),
+    path('<int:pk>/update/', update_view, name='update'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 import http
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.urls import reverse
 
@@ -29,3 +29,38 @@ def list_view(request):
     tasks = Task.objects.all().order_by('-pk')
 
     return render(request, 'tasks/list.html', {"tasks": tasks})
+
+
+def update_view(request, pk):
+    instance = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'Task update successfully')
+            return redirect(reverse('tasks:detail', kwargs={'pk': instance.pk}))
+
+        status = http.HTTPStatus.UNPROCESSABLE_ENTITY
+    else:
+        status = http.HTTPStatus.OK
+        form = TaskForm(instance=instance)
+
+    return render(request, 'tasks/update.html', {'form': form}, status=status)
+
+
+def detail_view(request, pk):
+    instance = get_object_or_404(Task, pk=pk)
+
+    return render(request, 'tasks/detail.html', {'task': instance})
+
+
+def delete_view(request, pk):
+    instance = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        instance.delete()
+
+        messages.success(request, 'Task deleted successfully')
+        return redirect('tasks:list')
+
+    return render(request, 'tasks/delete.html', {'instance': instance})
